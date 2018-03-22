1570e5d466321f97c13478ba0facb632a9a57ef5
lisashare <lisa_zhu2012@foxmail.com>
2018-03-22 20:25:41 +0800

    '完成了拖拽弹框项目'

--- js/drag.js
+++ js/drag.js
@@ -1,4 +1,5 @@
 class Drag{
+	
 	//里面添加原型方法
 	constructor(id){
 		//构造器
@@ -14,18 +15,35 @@ class Drag{
 		var target = e.target || e.srcElement;
 		var that = this;
 		if(target.id == "mouse_head"){
-			this.disX = e.clientX - this.ele.offsetLeft;
-			this.disY = e.clientY - this.ele.offsetTop;
+			this.disX = e.pageX - this.ele.offsetLeft;
+			this.disY = e.pageY - this.ele.offsetTop;
+			
 			document.onmousemove = function(evt){
 				that.fnMove(evt);
 			}
 			document.onmouseup = this.fnUp;
-			document.ondragStart = function(){
+			document.ondragstart = function(){
 				return false;
 			}
 		}
 	}
 	fnMove(evt){
-		
+		var e = evt || window.event;
+		var left = e.pageX - this.disX;
+		var top = e.pageY - this.disY;
+		if(left <= 0){left = 0;}
+		else if(left >= (document.documentElement.clientWidth - this.ele.offsetWidth) ){
+			left = document.documentElement.clientWidth - this.ele.offsetWidth;
+		}
+		if(top <= 0){
+			top = 0;
+		}else if(top >= document.documentElement.clientHeight - this.ele.offsetHeight){
+			top = document.documentElement.clientHeight - this.ele.offsetHeight;
+		}
+		this.ele.style.left = left + "px";
+		this.ele.style.top = top + "px";
+	}
+	fnUp(){
+		document.onmousemove = null;
 	}
 }
